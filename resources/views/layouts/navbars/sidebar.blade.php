83502c48a5cd76eaee6edd97df3f8401ef2a683e
farahhasnaoui <farah.hasnaoui@esprit.tn>
2023-12-10 22:13:14 +0100

    Project

---
+++ resources/views/layouts/navbars/sidebar.blade.php
@@ -0,0 +1,57 @@
+<nav class="sidenav navbar navbar-vertical  fixed-left  navbar-expand-xs navbar-light bg-white" id="sidenav-main">
+    <div class="scrollbar-inner">
+      <!-- Brand -->
+      <div class="sidenav-header  align-items-center">
+        <a class="navbar-brand" href="javascript:void(0)">
+        <img src="../assets/img/brand/blue.png" alt="Grand Logo"  >
+
+   
+        </a>
+      </div>
+      <div class="navbar-inner">
+        <!-- Collapse -->
+        <div class="collapse navbar-collapse" id="sidenav-collapse-main">
+          <!-- Nav items -->
+          <ul class="navbar-nav">
+            <li class="nav-item">          
+            </li>
+            <!-- <li class="nav-item">
+           
+
+                <div class="collapse show" id="navbar-examples">
+                    <ul class="nav nav-sm flex-column">
+                        <li class="nav-item">
+                            <a class="nav-link" >
+                 
+                                {{ __('User profile') }}
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" >
+                   
+                                {{ __('User Management') }}
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </li> -->
+
+          
+            <li class="nav-item">
+                <a class="nav-link" href="{{ route('table') }}">
+                  <i class="ni ni-bullet-list-67 text-default"></i>
+                  <span class="nav-link-text">Galeries d'art en ligne </span>
+                </a>
+              </li>
+     
+        </ul>
+          <!-- Divider -->
+          <hr class="my-3">
+          <!-- Heading -->
+   
+          <!-- Navigation -->
+     
+        </div>
+      </div>
+    </div>
+  </nav>
---
+++ resources/views/pages/form.blade.php
@@ -0,0 +1,40 @@
+<div class="modal fade" id="modalForm" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header text-center">
+                <h4 class="modal-title w-100 font-weight-bold">Add New</h4>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body mx-3">
+                <div class="md-form mb-5">
+             
+                    <input type="text" placeholder="test1" id="form34" class="form-control validate">
+                </div>
+
+                <div class="md-form mb-5">
+           
+                    <input type="email"  placeholder="test1"  id="form29" class="form-control validate">
+                </div>
+
+                <div class="md-form mb-5">
+           
+                    <input type="text" placeholder="test1"   id="form32" class="form-control validate">
+                </div>
+
+                <div class="md-form">
+            
+                    <textarea type="text" placeholder="test1"   id="form8" class="md-textarea form-control" rows="4"></textarea>
+                </div>
+            </div>
+            <div class="modal-footer d-flex justify-content-center">
+                <button class="btn btn-sm btn-neutral">Add <i class="fas fa-paper-plane-o ml-1"></i></button>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="text-right">
+    <a href="" class="btn btn-sm btn-neutral" data-toggle="modal" data-target="#modalForm">New</a>
+</div>
